76f921160090d6026e1274f8abf9e624e65c99cd
Julieta Campos <jcampos@itexico.net>
2019-09-28 23:56:27 +0200

    Change personal info and bio

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    title: `Juliescript`,
+    author: `Julieta Campos`,
+    description: `Personal blog by Julieta Campos, a Mexican Software Engineer based in Berlin.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `juliescript`,
+      instagram: `juliescript`
     },
   },
   plugins: [
